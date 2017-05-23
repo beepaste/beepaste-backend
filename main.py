9de5f822d0ab524850e226141eae33ee1b226455
Alireza Mosajjal <mosajjal@gmail.com>
2017-05-23 18:51:03 +0430

    added loger with an example

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import yaml
 from sanic import Sanic
 from sanic.response import json
 
-from tools import get_config
+from tools import get_config, get_logger
+
+logger = get_logger()
 web_cnf = get_config('web_server')
 
 # asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
@@ -14,4 +16,5 @@ async def test(request):
     return json({"hello": "world"})
 
 if __name__ == "__main__":
+    logger.debug('Starting server')
     app.run(**web_cnf, log_config=None)
--- tools.py
+++ tools.py
@@ -15,3 +15,12 @@ def get_config(section):
     except KeyError:
         #TODO: Log this
         return {}
+
+
+def get_logger():
+    import logging
+    from logging.config import dictConfig
+    logging_config = get_config('logger')
+    dictConfig(logging_config)
+    logger = logging.getLogger()
+    return logger
