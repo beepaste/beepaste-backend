c0f935a74d88e9fdb8ca6965a4cb9501f22d4df2
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-05-23 11:49:24 +0430

    mongodb init

---
+++ db/mongodb.py
@@ -0,0 +1,15 @@
+import asyncio
+import loop
+import motor.motor_asyncio
+
+class MongoDB(object):
+
+    def __init__(self, config, collection):
+        assert type(config) == dict
+        self.connString = 'mongodb://'
+        if 'username' in config:
+            self.connString += config['username'] + ':' + config['password'] + '@'
+        self.connString += config['host'] + ':' + config['port'] + '/' + config['database']
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(self.connString)
+        self.database = self.client[ config['database'] ]
+        self.collection = self.database[collection]
