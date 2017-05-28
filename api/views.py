232a103fcae829a723da73c1d9e27d31372361d0
Alireza Mosajjal <mosajjal@gmail.com>
2017-05-28 21:12:51 +0430

    added save to mongo

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 from tools import get_config, get_logger
-from models import paste
+from models.paste import Paste
+from db.mongodb import MongoDB
 from sanic.response import json
 
 
 async def new_paste(request):
     ''' saves a sent JSON object into database and returns a link to it '''
+    mongo_config = get_config('mongodb')
+    mongo_db = MongoDB(mongo_config, 'paste')
     input_json = request.json
     new_paste = Paste(**input_json)
+    res = mongo_db.insert(new_paste)
     # TODO: write saving to db and get the link
     return json({"hello": "world"})
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sanic.response import json
 from api import views, tools
 from tools import get_config, get_logger
 
-logger = get_logger()
+# logger = get_logger()
 web_cnf = get_config('web_server')
 
 # asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
@@ -14,5 +14,5 @@ app = Sanic()
 tools.add_api_routes(app)
 
 if __name__ == "__main__":
-    logger.info('Starting server')
-    app.run(**web_cnf)
+    # logger.info('Starting server')
+    app.run()
