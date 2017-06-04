c70a3b5224c9aa4cb064972e9a6f81b314fccf81
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-04 22:20:30 +0430

    added authorize route

--- api/tools.py
+++ api/tools.py
@@ -1,7 +1,8 @@
-from .views import new_paste
+from .views import new_paste, new_api_token
 
 
 def add_api_routes(app):
     app.add_route(new_paste, '/post/', methods=['POST'])
+    app.add_route(new_api_token, '/api/authorize', methods=['GET'])
     # app.add_route(handler2, '/folder/<name>')
     # app.add_route(person_handler2, '/person/<name:[A-z]>', methods=['GET'])
