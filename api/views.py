cee609a74324f45d896a03f71463c690a98613a2
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-04 22:20:47 +0430

    creating api token and return

--- api/views.py
+++ api/views.py
@@ -2,9 +2,11 @@ from tools import get_config, get_logger
 from models.paste import Paste
 from db.mongodb import MongoDB
 from sanic.response import json
+from models.api import Api
+import datetime
 
 
-async def new_paste(request):
+async def new_paste(request): # POST request
     ''' saves a sent JSON object into database and returns a link to it '''
     mongo_config = get_config('mongodb')
     mongo_db = MongoDB(mongo_config)
@@ -14,3 +16,17 @@ async def new_paste(request):
     new_paste.save(mongo_db)
     # TODO: write saving to db and get the link
     return json({"hello": "world"})
+
+async def new_api_token(request): # GET request
+    ''' generates new api-key if it was ok (no more than 5 tokens in 15minutes) '''
+    new_api = Api(expires=(datetime.datetime.utcnow() + datetime.timedelta(minutes=15)).timestamp(),
+                  ownerID=0, ip_address=request.ip)
+    mongo_config = get_config('mongodb')
+    dbEngine = MongoDB(mongo_config)
+    if new_api.countInTime(dbEngine, datetime.datetime.utcnow() - datetime.timedelta(minutes=15)) < 5:
+        new_api.genSecret()
+        new_api.genToken()
+        if new_api.is_valid:
+            new_api.save()
+            return json({'state': 'success', 'token': new_api['token']})
+    return json({'stat': 'received'})
