202e09cee2611240f3f0ecb7e0feb807ada38ee1
Alireza Mosajjal <mosajjal@gmail.com>
2017-06-05 14:31:22 +0430

    fixed fstring

--- db/mongodb.py
+++ db/mongodb.py
@@ -5,7 +5,7 @@ class MongoDB(object):
 
     def __init__(self, config):
         assert type(config) == dict
-        self.connString = f'mongodb://{config['username'}:config['password']@config['host']:str(config['port'])/config['database']'
+        self.connString = f"mongodb://{config['username']}:{config['password']}@{config['host']}:{str(config['port'])}/{config['database']}"
         self.client = motor.motor_asyncio.AsyncIOMotorClient(self.connString)
         self.database = self.client[config['database']]
 
