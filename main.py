38bec3dd699cebe48739192b469a5c758511b17a
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-08 15:30:23 +0430

    limiting each logfile to 50MB and saving 20 last files

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ logger = get_logger('beepaste')
 web_cnf = get_config('web_server')
 mongo_cnf = get_config('mongodb')
 
-# making connection to mongodb
 logger.info('initiating connection to mongodb using config')
 connect(**mongo_cnf)
 logger.info('connected to mongodb')
 
-# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-app = Sanic()
+app = Sanic('beepaste')
 
 tools.add_api_routes(app)
 
