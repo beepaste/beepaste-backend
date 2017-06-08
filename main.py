70ddbce5c2f403cb98e93e6b93f2873f38210cc1
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-08 13:21:38 +0430

    added logger!

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from SanicMongo import connect
 from api import views, tools
 from tools import get_config, get_logger
 
-# logger = get_logger()
+logger = get_logger('beepaste')
+
 web_cnf = get_config('web_server')
 mongo_cnf = get_config('mongodb')
 
 # making connection to mongodb
+logger.info('initiating connection to mongodb using config')
 connect(**mongo_cnf)
+logger.info('connected to mongodb')
 
 # asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = Sanic()
@@ -19,5 +22,5 @@ app = Sanic()
 tools.add_api_routes(app)
 
 if __name__ == "__main__":
-    # logger.info('Starting server')
+    logger.info('Starting server')
     app.run(**web_cnf)
