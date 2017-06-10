bfa3906317f2ad37ca02ea9b17d68efc23d32711
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-10 23:36:34 +0430

    connecting to mongo after server start

--- beepaste/__init__.py
+++ beepaste/__init__.py
@@ -1,5 +1,4 @@
 from sanic import Sanic
-from SanicMongo import connect
 from beepaste.utils.config import get_config
 from beepaste.utils.logger import get_logger
 from beepaste.modules import moduleApiV1
@@ -9,12 +8,6 @@ logger = get_logger('beepaste')
 # config
 web_cnf = get_config('web_server')
 mongo_cnf = get_config('mongodb')
-
-# db init
-logger.info('initiating connection to mongodb using config')
-connect(**mongo_cnf)
-logger.info('connected to mongodb')
-
 
 def app():
     # load sanic application
--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 from beepaste import app
-from beepaste import web_cnf
+from beepaste import web_cnf, mongo_cnf
 from beepaste import logger
+from SanicMongo import connect
+
+
+
+async def notify_server_started():
+    # db init
+    logger.info('initiating connection to mongodb using config')
+    connect(**mongo_cnf)
+    logger.info('connected to mongodb')
 
 if __name__ == "__main__":
     logger.info('Starting server')
     application = app()
+    application.add_task(notify_server_started())
     application.run(**web_cnf)
