6a2c900700261a4538c439ac98e359f0badfb8f4
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-10 20:42:29 +0430

    removed calls object

--- beepaste/modules/apiv1/api.py
+++ beepaste/modules/apiv1/api.py
@@ -21,30 +21,23 @@ class Api(Document):
     ownerID = IntField(default=0)
     ip_address = StringField(required=True)
     generated_on = DateTimeField(default=datetime.datetime.utcnow())
-    calls = DictField(required=True, default={
-        'create_paste': 450,
-        'get_paste': 450,
-        'delete_pastes': 450,
-        'create_user': 3,
-        'authenticate_user': 5,
-        'get_user_pastes': 90
-    })                                  #'''
-                                        #    Here we store calls remainig for each token!
-                                        #    and they are reseted every 15 minutes.
-                                        #    defaults are:
-                                        #    {
-                                        #        create_paste: 450,
-                                        #        get_paste: 450,
-                                        #        delete_pastes: 450,
-                                        #        create_user: 3,
-                                        #        authenticate_user: 5,
-                                        #        get_user_pastes: 90,
-                                        #    }
-                                        #'''
+    #'''
+    #    Here we store calls remainig for each token!
+    #    and they are reseted every 15 minutes.
+    #    defaults are:
+    #    {
+    #        create_paste: 450,
+    #        get_paste: 450,
+    #        delete_pastes: 450,
+    #        create_user: 3,
+    #        authenticate_user: 5,
+    #        get_user_pastes: 90,
+    #    }
+    #'''
 
     meta = {'collection': 'tokens'}
 
-    def genSecret(self, len=8):
+    def genSecret(self, len=16):
         allchar = string.ascii_letters + string.digits
         self.secret = "".join(choice(allchar) for x in range(len))
 
