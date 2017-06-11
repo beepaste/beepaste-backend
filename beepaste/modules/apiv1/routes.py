0100160a5d49b3e59a5d71bacba180ea27ade40f
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-11 11:28:56 +0430

    fixed routes

--- beepaste/modules/apiv1/routes.py
+++ beepaste/modules/apiv1/routes.py
@@ -2,8 +2,8 @@ from .views import get_paste, post_paste, new_api_token
 
 
 def add_api_routes(app):
-    app.add_route(get_paste, '/', methods=['GET'])
-    app.add_route(post_paste, '/', methods=['POST'])
-    app.add_route(new_api_token, '/api/authorize', methods=['GET'])
+    app.add_route(get_paste, '/paste', methods=['GET'])
+    app.add_route(post_paste, '/paste', methods=['POST'])
+    app.add_route(new_api_token, '/authorize', methods=['GET'])
     # app.add_route(handler2, '/folder/<name>')
     # app.add_route(person_handler2, '/person/<name:[A-z]>', methods=['GET'])
