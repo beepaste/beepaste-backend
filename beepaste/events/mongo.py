83512446c032a1af676de7fd5d6fa37835fead8b
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-11 11:27:55 +0430

    removed one info log

--- beepaste/events/mongo.py
+++ beepaste/events/mongo.py
@@ -7,6 +7,5 @@ from beepaste import app
 @app.listener('after_server_start')
 async def notify_mongo_started(app, loop):
     # db init
-    logger.info('initiating connection to mongodb using config')
     connect(**mongo_cnf)
     logger.info('connected to mongodb')
