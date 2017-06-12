152976acaddfc9c73692b3f493e276d71147e9c8
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-13 01:11:41 +0430

    added todo

--- beepaste/events/mongo.py
+++ beepaste/events/mongo.py
@@ -1,5 +1,4 @@
 from SanicMongo import connect
-from beepaste import logger
 from beepaste import mongo_cnf
 from beepaste import app
 
@@ -7,5 +6,5 @@ from beepaste import app
 @app.listener('after_server_start')
 async def notify_mongo_started(app, loop):
     # db init
+    # TODO: write a logger to write logs to database!
     connect(**mongo_cnf)
-    logger.info('connected to mongodb')
