4f3175ee22f64b48bc23ab9af73326c5c52f1e7b
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-13 01:11:33 +0430

    added todo

--- beepaste/events/analytic.py
+++ beepaste/events/analytic.py
@@ -4,7 +4,7 @@ from random import randint
 
 @app.middleware('request')
 async def add_analytic_to_session(request):
-    # TODO save some data to session on redis db
+    # TODO remove this shit :)
     if not request['stat'].get('counter'):
         request['stat']['counter'] = 0
     request['stat']['ip'] = request.ip
