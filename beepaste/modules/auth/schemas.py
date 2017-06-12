2db250cef540097494cacf59ff7d326320450510
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-12 10:47:14 +0430

    receiving username instead of email

--- beepaste/modules/auth/schemas.py
+++ beepaste/modules/auth/schemas.py
@@ -2,9 +2,9 @@ from marshmallow import Schema, fields
 
 
 class loginSchema(Schema):
-    email = fields.Email(
+    username = fields.Str(
             required=True,
-            error_messages={'required': 'email is required.'}
+            error_messages={'required': 'username is required.'}
             )
     password = fields.Str(
             required=True,
