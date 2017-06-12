d6861154eda691942bae7988a905ed06a78ab93f
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-13 01:01:23 +0430

    handling exceptions for jwt decode

--- beepaste/events/auth.py
+++ beepaste/events/auth.py
@@ -1,13 +1,24 @@
 from beepaste import app, jwt_cnf
 import jwt
+from jwt.exceptions import (DecodeError, ExpiredSignatureError)
+from sanic import response
 
 
 @app.middleware('request')
 async def checkAuth(request):
     if request.headers.get('X-TOKEN'):
         encoded = request.headers['X-TOKEN']
-        decodetoken = jwt.decode(encoded, jwt_cnf['secret'],
-                                 algorithm=jwt_cnf['algorithm'])
+        try:
+            decodetoken = jwt.decode(encoded, jwt_cnf['secret'],
+                                     algorithm=jwt_cnf['algorithm'])
+        except DecodeError as e:
+            return response.json(
+                {'status': 'fail', 'details': 'invalid token'},
+                status=400)
+        except ExpiredSignatureError as e:
+            return response.json(
+                {'status': 'fail', 'details': 'token expired'},
+                status=400)
         # TODO check if token not decoded or validate some response to user
         request['userid'] = decodetoken['userid']
     else:
