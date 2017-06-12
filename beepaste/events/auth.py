4a73672f26275ebe6444bcef33f6963040e50793
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-12 10:41:24 +0430

    generating jwt token from config!

--- beepaste/events/auth.py
+++ beepaste/events/auth.py
@@ -1,4 +1,4 @@
-from beepaste import app
+from beepaste import app, jwt_cnf
 import jwt
 
 
@@ -6,7 +6,8 @@ import jwt
 async def checkAuth(request):
     if request.headers.get('X-TOKEN'):
         encoded = request.headers['X-TOKEN']
-        decodetoken = jwt.decode(encoded, 'secret', algorithms=['HS256'])
+        decodetoken = jwt.decode(encoded, jwt_cnf['secret'],
+                                 algorithm=jwt_cnf['algorithm'])
         # TODO check if token not decoded or validate some response to user
         request['userid'] = decodetoken['userid']
     else:
