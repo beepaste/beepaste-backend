669cedfd99c7c2f9ea0a95e8db774a9e0af2b59b
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-06-12 10:47:00 +0430

    cleanup and hashing passwords!

--- beepaste/modules/auth/models.py
+++ beepaste/modules/auth/models.py
@@ -1,24 +1,38 @@
 from SanicMongo import Document
-from SanicMongo.fields import (IntField, DateTimeField, StringField, URLField,
-                               BooleanField)
+from SanicMongo.fields import (EmailField, DateTimeField, StringField, IntField)
+import datetime
+import json
+from passlib.hash import sha512_crypt
 
 
 class UserModel(Document):
 
-    email = StringField(required=True, min_length=6, max_length=127)
+    userid = IntField(required=True, primary_key=True)
+    email = EmailField(required=True, min_length=6, max_length=127, unique=True)
     password = StringField(required=True, min_length=6, max_length=127)
+    username = StringField(required=True, min_length=6, max_length=30,
+                           unique=True)
+    registered = DateTimeField(default=datetime.datetime.utcnow())
 
-    async def authorize(email, password):
-        # TODO check user by email and pass if success return userid else none
-        if email == 'admin@beepaste.com' and password == "admin":
-            userid = 1
+    async def authorize(username, password):
+        curr_user = await UserModel.objects(username=username).first()
+        if curr_user is not None:
+            if sha512_crypt.verify(password, curr_user.password):
+                userid = curr_user.userid
+            else:
+                userid = None
             return userid
         else:
             return None
 
     async def getById(userid):
-        # TODO get user id from mongodb and return safe parameter
-        return {
-                "name": "beepaste",
-                "somthing": "somedata from user"
-                }
+        curr_user = await UserModel.objects(username=username).first()
+        if curr_user is not None:
+            ret_data_json = curr_user.to_json()
+            ret_data = json.loads(ret_data_json)
+            return ret_data
+        else:
+            return None
+
+    async def setPassword(self, password):
+        self.password = sha512_crypt.hash(password)
