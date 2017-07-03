2243c5cde5ac77dabb863c4994584b3f10dacb05
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-07-03 12:06:27 +0430

    functions to handle redis actions!

--- beepaste/redis.py
+++ beepaste/redis.py
@@ -1,6 +1,7 @@
 import asyncio_redis
 from beepaste.utils.logger import lg
 from beepaste import redis_cnf
+import json
 
 
 class Redis:
@@ -9,6 +10,27 @@ class Redis:
     pool across your application.
     """
     _pool = None
+
+    async def set_value(self, key, data):
+        conn = await self.get_redis_pool()
+        await conn.set(key, data)
+
+    async def get_value(self, key):
+        conn = await self.get_redis_pool()
+        data = await conn.get(key)
+
+    async def get_dict(self, key):
+        # get str and convert to json
+        value = await self.get_value(key)
+
+        data = json.loads(value)
+        return data
+
+    async def set_dict(self, key, data):
+        # set value of key, a dict object!
+        value = json.dumps(data)
+
+        await self.set_value(key, value)
 
     async def get_redis_pool(self):
         if not self._pool:
