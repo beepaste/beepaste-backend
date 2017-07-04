803f76544bd4ef2dd23d7ab607ee41fe4f9a0dea
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-07-04 18:50:44 +0430

    switched to aioredis

--- beepaste/events/auth.py
+++ beepaste/events/auth.py
@@ -1,7 +1,8 @@
-from beepaste import app, jwt_cnf
+from beepaste import app, jwt_cnf, limits_cnf
 import jwt
 from jwt.exceptions import (DecodeError, ExpiredSignatureError)
 from sanic import response
+from beepaste.redis import redis
 
 
 @app.middleware('request')
@@ -9,8 +10,40 @@ async def checkAuth(request):
     if request.headers.get('X-TOKEN'):
         encoded = request.headers['X-TOKEN']
         try:
-            decodetoken = jwt.decode(encoded, jwt_cnf['secret'],
+            decodedtoken = jwt.decode(encoded, jwt_cnf['secret'],
                                      algorithm=jwt_cnf['algorithm'])
+
+            if decodedtoken['userid'] == 0:
+                source_string = decodedtoken['ip'] + '_limits'
+            else:
+                source_string = decodedtoken['userid'] + '_limits'
+
+            token_stat = await redis.get_value(encoded + '_stat')
+
+            if token_stat != 'valid':
+                return response.json(
+                    {'status': 'fail', 'details': 'invalid token2 ' + token_stat},
+                    status=403)
+
+            token_limits_exists = await redis.exists(encoded + '_limits')
+            source_limits_exists = await redis.exists(source_string)
+
+            if not token_limits_exists:
+                await redis.set_dict(encoded + '_limits', limits_cnf)
+                await redis.expire(encoded + '_limits', limits_cnf['reset_timeout'])
+
+            if not source_limits_exists:
+                await redis.set_dict(source_string, limits_cnf)
+                await redis.expire(source_string, limits_cnf['reset_timeout'])
+
+            token_limits = await redis.get_dict(encoded + '_limits')
+            source_limits = await redis.get_dict(source_limits)
+
+            request['token_limits'] = token_limits
+            request['source_limits'] = source_limits
+
+            request['userid'] = decodedtoken['userid']
+
         except DecodeError as e:
             return response.json(
                 {'status': 'fail', 'details': 'invalid token'},
@@ -19,6 +52,5 @@ async def checkAuth(request):
             return response.json(
                 {'status': 'fail', 'details': 'token expired'},
                 status=400)
-        request['userid'] = decodetoken['userid']
     else:
         request['userid'] = None
