8bdc25da75e6a55742d063f6faf8a182b1ec09b4
MohammadAmin Vahedinia <mohammadamin_github@vahedinia.me>
2017-08-05 01:26:45 +0430

    initializing streaming api

--- beepaste/modules/v1/__init__.py
+++ beepaste/modules/v1/__init__.py
@@ -1,9 +1,16 @@
 from sanic import Blueprint
 from beepaste.modules.v1.routes.paste import init_paste_routes
 from beepaste.modules.v1.routes.auth import init_auth_routes
+import socketio
+from beepaste import app
 
 # init modules
 moduleApiV1 = Blueprint('api/v1', url_prefix='/api/v1')
+
+sio = socketio.AsyncServer(async_mode='sanic')
+sio.attach(app)
+
+from beepaste.modules.v1.views import streaming
 
 # init modules routes # TODO improve name
 init_paste_routes(moduleApiV1)
