7bcb6d09b99d3cfdbc9fa8900ed7fb0c80c274a6
mostafa shaheydari <shaheydari.mostafa@gmail.com>
2017-12-03 22:31:31 +0330

    Cors issue is fixed

--- beepaste/modules/v1/__init__.py
+++ beepaste/modules/v1/__init__.py
@@ -1,9 +1,12 @@
 from sanic import Blueprint
+from sanic_cors import CORS
+
 from beepaste.modules.v1.routes.paste import init_paste_routes
 from beepaste.modules.v1.routes.auth import init_auth_routes
 
 # init modules
 moduleApiV1 = Blueprint('api/v1', url_prefix='/api/v1')
+CORS(moduleApiV1,automatic_options=True)
 
 # init modules routes # TODO improve name
 init_paste_routes(moduleApiV1)
