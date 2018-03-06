faf328b70209f6b217bcc610534775dbf7f6daab
MohammadAmin Vahedinia <github@vahedinia.me>
2018-03-06 15:08:42 +0330

    fixed test issues!

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,21 @@
 import pytest
+import asyncio
 from beepaste import app
 from sanic.websocket import WebSocketProtocol
 
 @pytest.yield_fixture
-def app_server():
+def app_srv():
     yield app
 
 @pytest.fixture
-def test_cli(loop, app_server, test_client):
-    return loop.run_until_complete(test_client(app, protocol=WebSocketProtocol))
+def test_cli(loop, app_srv, test_client):
+    return loop.run_until_complete(test_client(app_srv, protocol=WebSocketProtocol))
+
+@pytest.fixture
+def sanic_server(loop, app_srv, test_server):
+    return loop.run_until_complete(test_server(app_srv))
+
+@pytest.fixture
+def loop():
+    loop = asyncio.get_event_loop()
+    yield loop
